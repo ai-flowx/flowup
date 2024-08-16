6c4569a6a1a6c6204a0c37c7ba5670bf347e7230
Jia <49358172+craftslab@users.noreply.github.com>
2024-08-16 04:03:33 +0000

    feat(show): add support to show toolchain version

--- cmd/show.go
+++ cmd/show.go
@@ -3,7 +3,9 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"os/exec"
 	"path/filepath"
+	"strings"
 
 	"github.com/fatih/color"
 	"github.com/shirou/gopsutil/v3/host"
@@ -11,6 +13,10 @@ import (
 	"github.com/spf13/viper"
 
 	"github.com/cligpt/shup/config"
+)
+
+const (
+	versionLen = 3
 )
 
 var showCmd = &cobra.Command{
@@ -37,40 +43,77 @@ func init() {
 }
 
 func runShow(_ *config.Config) error {
-	info, _ := hostInfo()
+	hostInfo, _ := fetchHost()
 
 	color.Set(color.Bold)
-	fmt.Printf("host: ")
+	fmt.Print("host: ")
 	color.Unset()
-	fmt.Printf("%s\n", info)
+	fmt.Printf("%s\n", hostInfo)
 
 	home, _ := os.UserHomeDir()
 	path := filepath.Join(home, config.RootName)
 
 	color.Set(color.Bold)
-	fmt.Printf("home: ")
+	fmt.Print("home: ")
 	color.Unset()
 	fmt.Printf("%s\n\n", path)
 
 	color.Set(color.Bold)
-	fmt.Printf("installed toolchains\n")
-	fmt.Printf("--------------------\n\n")
+	fmt.Print("installed toolchains\n")
+	fmt.Print("--------------------\n\n")
 	color.Unset()
 
-	fmt.Printf("shai 1.0.0\n")
-	fmt.Printf("gitgpt 1.0.0\n")
-	fmt.Printf("lintgpt 1.0.0\n")
-	fmt.Printf("metalgpt 1.0.0\n")
+	path = filepath.Join(home, config.BinName)
+	toolchainInfo, _ := fetchToolchain(path)
+
+	for _, item := range toolchainInfo {
+		fmt.Println(item)
+	}
+
 	fmt.Printf("\n")
 
 	return nil
 }
 
-func hostInfo() (string, error) {
+func fetchHost() (string, error) {
 	info, err := host.Info()
 	if err != nil {
 		return "unknown", err
 	}
 
 	return fmt.Sprintf("%s-%s-%s", info.Platform, info.PlatformVersion, info.KernelArch), nil
+}
+
+func fetchToolchain(path string) ([]string, error) {
+	helper := func(file string) (string, error) {
+		cmd := exec.Command(file, "--version")
+		out, err := cmd.CombinedOutput()
+		if err != nil {
+			return "", err
+		}
+		buf := strings.Split(string(out), " ")
+		if len(buf) != versionLen {
+			return "", err
+		}
+		// shup version v1.0.0-build-2024-08-16T11:33:19+0800
+		name := buf[0]
+		version := strings.TrimPrefix(strings.Split(buf[2], "-")[0], "v")
+		return fmt.Sprintf("%s %s", name, version), nil
+	}
+
+	var files []string
+	var buf []string
+
+	_ = filepath.Walk(path, func(p string, _ os.FileInfo, _ error) error {
+		files = append(files, p)
+		return nil
+	})
+
+	for _, item := range files {
+		if b, err := helper(item); err == nil {
+			buf = append(buf, b)
+		}
+	}
+
+	return buf, nil
 }
--- cmd/show_test.go
+++ cmd/show_test.go
@@ -6,7 +6,14 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestHostInfo(t *testing.T) {
-	_, err := hostInfo()
+func TestFetchHost(t *testing.T) {
+	_, err := fetchHost()
 	assert.Equal(t, nil, err)
+}
+
+func TestFetchToolchain(t *testing.T) {
+	buf, err := fetchToolchain("../test/install")
+	assert.Equal(t, nil, err)
+	assert.Equal(t, "shai 1.0.0", buf[0])
+	assert.Equal(t, "shup 1.0.0", buf[1])
 }
